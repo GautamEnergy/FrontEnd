5114e8ccd7ab188cf11a9e2bed02f1bc95b99f2e
GautamEnergy <157204115+GautamEnergy@users.noreply.github.com>
2024-01-23 18:42:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./Components/Navbar";
 import RenderCard from "./RenderCard";
 import Pagination from "./Components/Pagination";
 function App() {
+  console.log("app")
   return (
     <> <Navbar/>
     <RenderCard/>
